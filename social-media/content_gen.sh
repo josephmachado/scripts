13d1ac404012f664cdf55b4157db2b95bb4431a0
JosephKevinMachado <joseph.machado.professional@gmail.com>
2025-05-26 11:49:20 -0400

    2025-05-26-11-49-20 - formatting

--- social-media/content_gen.sh
+++ social-media/content_gen.sh
@@ -33,6 +33,12 @@ fi
 
 echo "Creating prompt for $INPUT_TOPIC, from $INPUT_WEBSITE_URL, optionally for the subsection $INPUT_SUBSECTION"
 
+echo "----------------------------------------------------------------------------------------------------------------------------------
+
+
+
+"
+
 PROMPT_FILE="$HOME/.local/bin/scripts/social-media/linkedin/topic_based_post_template.template"
 
 sed "s|INPUT_TOPIC|$INPUT_TOPIC|g; s|INPUT_WEBSITE_URL|$INPUT_WEBSITE_URL|g; s|INPUT_SUBSECTION|$INPUT_SUBSECTION|g" $PROMPT_FILE
